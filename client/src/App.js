69374833ac9dd31f72fc1349ae5cb2b3d847dd13
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-12-04 19:48:22 +0300

    newswrite component finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import Main from "./components/Main"
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Main from "./components/Main";
+import NewsWrite from "./components/subcomponents/NewsWrite";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      
+      <Router>
+        <Routes>
+          <Route path="/newswrite" element={<NewsWrite/>} />
+          <Route path="/" element={<Main/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/components/subcomponents/NewsWrite.js
+++ client/src/components/subcomponents/NewsWrite.js
@@ -1,58 +1,85 @@
-import React, { useState } from "react";
-import { keccak256 } from "js-sha3";
+import React, {useState} from "react";
+import axios from "axios";
+import {keccak256} from "js-sha3";
 
-const NewsWrite = () => {
-  const [password, setPassword] = useState("");
-  const [newsContent, setNewsContent] = useState("");
-  const [response, setResponse] = useState("");
-
+function NewsWrite() {
+  const [reporter, setReporter] = useState("");
+  const [party, setParty] = useState("");
+  const [newsText, setNewsText] = useState("");
+  const [datetime, setDatetime] = useState(null);
+  const [source, setSource] = useState("");
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [resultArea, setResultArea] = useState(null);
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // Hash the password using Keccak-256
-    const hashedPassword = keccak256(password);
-
+    const reporter2 = keccak256(reporter);
     try {
-      const res = await fetch("http://localhost:5000/api/save-news", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ hashedPassword, newsContent }),
-      });
+      const newsObject = {
+        reporter2,
+        party,
+        newsText,
+        datetime,
+        source,
+        latitude,
+        longitude,
+      };
+      console.log(newsObject);
 
-      const data = await res.json();
-      setResponse(data.message);
+/*       const res = await axios.post("http://localhost:5000/api/save-news", newsObject);
+      setResponse(res.data.message); */
     } catch (error) {
-      setResponse("An error occurred.");
+      console.log(error.message)
+/*       if (error.response) {
+        setResponse(error.response.data.message);
+        console.log(error.message);
+      } else {
+        setResponse("An error happened while saving the news");
+        console.log(error.message);
+      } */
     }
-  };
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <label htmlFor="inputParty">Party:</label>
+          <input type="text" id="inputParty" value={party} onChange={(e) => setParty(e.target.value)} required />
         </div>
         <div>
-          <label>News Content:</label>
-          <textarea
-            value={newsContent}
-            onChange={(e) => setNewsContent(e.target.value)}
-            required
-          ></textarea>
+          <label htmlFor="inputNews">News:</label>
+          <textarea type="text" id="inputNews" value={newsText} onChange={(e) => setNewsText(e.target.value)} required >
+          </textarea>
+        </div>
+        <div>
+          <label htmlFor="inputDate">Select Date and Time:</label>
+          <input type="datetime-local" id="inputDate" value={datetime} onChange={(e) => setDatetime(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="inputSource">Source link:</label>
+          <input type="text" id="inputSource" value={source} onChange={(e) => setSource(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="inputLatitude">Latitude:</label>
+          <input type="number" step="0.000001" id="inputLatitude" 
+            value={latitude} onChange={(e) => setLatitude(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="inputLongitude">Longitude:</label>
+          <input type="number" step="0.000001" id="inputLongitude" 
+            value={longitude} onChange={(e) => setLongitude(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="inputReporter">Reporter:</label>
+          <input type="text" id="inputReporter" value={reporter} onChange={(e) => setReporter(e.target.value)} required />
         </div>
         <button type="submit">Save News</button>
       </form>
-      {response && <p>{response}</p>}
+      <div>{resultArea}</div>
     </div>
   );
-};
+}
 
-export default NewsWrite;
+export default NewsWrite
